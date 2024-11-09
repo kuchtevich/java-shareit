e0e6df4bd18872676ae2672defb589b38d948d7f
User Daria <kuchtevich@yandex.tu>
2024-11-09 17:07:49 +0300

    ТЗ-16, доработка модуля server и разработка модуля gateway

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -10,6 +10,7 @@ public class ItemDto {
         private String name;
         private String description;
         private Boolean available;
+        private Long requestId;
         private BookingDtoTime lastBooking;
         private BookingDtoTime nextBooking;
 }
